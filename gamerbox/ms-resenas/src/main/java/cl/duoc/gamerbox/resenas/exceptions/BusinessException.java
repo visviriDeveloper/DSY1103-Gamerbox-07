319309eb76c7970ed45d7ccd46c41eaaacdd7298
Matias2772 <ma.wenger@duocuc.cl>
2026-05-17 18:28:24 -0400

    feat: added dockerfile, config and exceptions

---
+++ gamerbox/ms-resenas/src/main/java/cl/duoc/gamerbox/resenas/exceptions/BusinessException.java
@@ -0,0 +1,7 @@
+package cl.duoc.gamerbox.resenas.exceptions;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String mensaje) {
+        super(mensaje);
+    }
+}
