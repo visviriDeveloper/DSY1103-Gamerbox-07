2cf61a240823a8e8eb7c9b674ce88a2835801aca
visviriDeveloper <al.moralesy@duocuc.cl>
2026-05-18 12:23:01 -0400

    fix init.sql

---
+++ gamerbox/init.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE gamerbox_usuarios;
+CREATE DATABASE gamerbox_juegos;
+CREATE DATABASE gamerbox_resenas;
+CREATE DATABASE gamerbox_listas;
+CREATE DATABASE gamerbox_comentarios;
+CREATE DATABASE gamerbox_seguidores;
+CREATE DATABASE gamerbox_feed;
+CREATE DATABASE gamerbox_login;
